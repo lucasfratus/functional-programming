659c8cb5de0d5ea6fe28d771924e1ea832318d4a
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-10-16 14:55:04 -0300

    Metodo de substituição adicionado nos exercicios 21 e 22

--- fund-02/ex21.rkt
+++ fund-02/ex21.rkt
@@ -1,3 +1,11 @@
 #lang racket
 (define (produto-anterior-posterior n)
     (* (n-1) n (n+1)))
+
+;; Metodo de substituição
+;; > (produto-anterior-posterior 3)
+;; (* (3-1) 3 (3+1))
+;; (* 2 3 (3+1))
+;; (* 2 3 4)
+;; (* 6 4)
+;; 24
--- fund-02/ex22.rkt
+++ fund-02/ex22.rkt
@@ -1,4 +1,15 @@
 #lang racket
 (define (so-primeira-maiuscula str)
-    (string-append (string-upcase(substring 0 1))
+    (string-append (string-upcase(substring str 0 1))
         (string-downcase(substring str 1 (string-length str)))))
+
+;; Metodo de substituição
+;; > (so-primeira-maiuscula "paula")
+;;   (string-append (string-upcase(substring "paula" 0 1))
+;;        (string-downcase(substring "paula" 1 (string-length paula))))
+;; (substring "paula" 0 1) ;; "p"
+;; (string-upcase "p" ) ;; "P"
+;; (substring "paula" 1 5) ;; "aula"
+;; (string-downcase "aula") ;; "aula"
+;; (string-append "P" "aula") ;; "Paula"
+;; "Paula"
